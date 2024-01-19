251a41c95558bde8f21d06c8471771f2f6bf2adc
陈蕃坊 <493336001@qq.com>
2024-01-19 14:59:18 +0800

    插入排序

---
+++ Python刷题/排序/插入排序.py
@@ -0,0 +1,22 @@
+from typing import List
+def sort(arr: List[int]):
+    for i in range(1, len(arr)):
+        # 新的数字
+        value = arr[i]
+        # 已排过序的下标
+        j = i - 1
+        while j >= 0 and arr[j] > value:
+            # 新的数向左移，那么也就是旧的数向右移动
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = value
+
+
+def test():
+    arr = [5, 3, 4, 2, 6, 1]
+    sort(arr=arr)
+    print(arr)
+
+
+test()
